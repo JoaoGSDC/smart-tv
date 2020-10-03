5b253f2d073527614ad859a8f71e15b0cf0e1ca0
JoaoGSDC <joaogsdc@gmail.com>
2020-10-03 00:41:43 -0300

    Project #1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Routes from './routes';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <a href="https://www.google.com.br/">
+          <div className="container-site">
+            <img
+              className="img-site-google"
+              src="https://imagepng.org/wp-content/uploads/2019/08/google-icon-1.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="http://www.youtube.com/tv">
+          <div className="container-site">
+            <img
+              className="img-site-yt"
+              src="https://mobilebit.b-cdn.net/wp-content/uploads/2020/04/youtube-tv-nova-atualizacao.jpg"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.netflix.com/br/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://www.rvtecnologia.com.br/wp-content/uploads/2014/11/Netflix-Logo.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.twitch.tv/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Twitch_logo_2019.svg/800px-Twitch_logo_2019.svg.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.telecineplay.com.br/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://logodownload.org/wp-content/uploads/2017/07/telecine-logo-8.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.hbogo.com.br/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://cdn.iconscout.com/icon/free/png-512/hbo-go-2-569347.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.primevideo.com/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Amazon_Prime_Video_logo.svg/1200px-Amazon_Prime_Video_logo.svg.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.eiplus.com.br/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://logodownload.org/wp-content/uploads/2018/10/esporte-interativo-logo-0.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.espn.com.br/watch/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://logodownload.org/wp-content/uploads/2015/05/espn-logo-4.png"
+              alt="img"
+            />
+          </div>
+        </a>
+
+        <a href="https://www.discoverykidsplus.com.br/">
+          <div className="container-site">
+            <img
+              className="img-site"
+              src="https://logodownload.org/wp-content/uploads/2018/04/discovery-kids-logo.png"
+              alt="img"
+            />
+          </div>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Home;
